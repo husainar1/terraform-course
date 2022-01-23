1f556baeb1fb1dc886028f627448317696ec4103
husainar1 <96575746+husainar1@users.noreply.github.com>
2022-01-23 13:52:22 +0400

    Update providers.tf

--- 06_vm_linux/providers.tf
+++ 06_vm_linux/providers.tf
@@ -6,7 +6,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.78.0"
+      version = "2.93.0"
+      skip_provider_registration = false
     }
   }
 }
